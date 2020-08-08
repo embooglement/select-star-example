80524ff0be217feae36e4aff6f31183c3d19c115
Kevin Beason <kevin.beason@addepar.com>
2020-08-08 13:21:19 -0400

    Really janky UI for editing tree view

---
+++ frontend/components/Directory.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const DirectoryWrapper = styled.div`
+  border: 1px solid black;
+  padding: 8px;
+`;
+
+const DirectoryTitle = styled.h1`
+  margin: 4px;
+`;
+
+const DirectoryList = styled.ol`
+
+`;
+
+const DirectoryListItem = styled.li`
+
+`;
+
+const DirectoryListItemName = styled.span`
+  margin-right: 8px;
+`;
+
+const DirectoryListItemControls = styled.span`
+
+`;
+
+type Props = {
+  directory: string,
+  items: string[],
+  addItemToNewDirectory: (item: string, directory: string, newDirectory: string) => void,
+};
+
+export default function Directory(props: Props) {
+  const [items, setItems] = useState(props.items);
+
+  return (
+    <DirectoryWrapper>
+      <DirectoryTitle>{props.directory} ({items.length})</DirectoryTitle>
+      <DirectoryList>
+        {items.map((item) => {
+          const [newDirectoryName, setNewDirectoryName] = useState(null);
+          const moveDirectory = () => {
+            props.addItemToNewDirectory(item, props.directory, newDirectoryName.trim());
+          };
+
+          return (
+            <DirectoryListItem key={item}>
+              <DirectoryListItemName>{item}</DirectoryListItemName>
+              <DirectoryListItemControls>
+                <input type="text"
+                  placeholder="New directory"
+                  onChange={(e) => {
+                    if (e.target.value) {
+                      setNewDirectoryName(e.target.value);
+                    }
+                  }}
+                />
+                <button onClick={moveDirectory}>Submit</button>
+              </DirectoryListItemControls>
+            </DirectoryListItem>
+          );
+        })}
+      </DirectoryList>
+    </DirectoryWrapper>
+  );
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,7 @@
-import Head from 'next/head';
+import { useState } from 'react';
+import styled from 'styled-components';
+import Directory from '../components/Directory';
+
 
 type HomeProps = {
   [nameGroup: string]: string[];
@@ -17,20 +20,34 @@ export async function getStaticProps() {
   };
 }
 
-// TODO: use nicer wrapper component for the tree view
-
 export default function Home({ nameGroupings }: HomeProps) {
+  const [directories, setDirectories] = useState(nameGroupings);
+
+  const addItemToNewDirectory = (item: string, directory: string, newDirectory: string) => {
+    const newDirectories = {};
+    for (const directoryToCopy of Object.keys(directories)) {
+      newDirectories[directoryToCopy] = [...directories[directoryToCopy]];
+    }
+
+    newDirectories[directory] = newDirectories[directory].filter((otherItem) => item !== otherItem);
+
+    if (typeof newDirectories[newDirectory] !== 'undefined') {
+      newDirectories[newDirectory].push(item);
+    } else {
+      newDirectories[newDirectory] = [item];
+    }
+
+    setDirectories(newDirectories);
+  };
+
   return (
     <ol>
-      {Object.keys(nameGroupings).map((nameGroup) => {
+      {Object.keys(directories).map((directory) => {
+        const items = directories[directory] || [];
+        // TODO: come up with a better key prop
         return (
-          <li>
-            <h2>{nameGroup}</h2>
-            <ol>
-              {nameGroupings[nameGroup].map((name) => {
-                return <li>{name}</li>;
-              })}
-            </ol>
+          <li key={directory + " " + items.length}>
+            <Directory directory={directory} items={items} addItemToNewDirectory={addItemToNewDirectory} />
           </li>
         );
       })}
